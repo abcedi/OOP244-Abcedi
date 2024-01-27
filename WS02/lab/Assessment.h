6ecd1133fcdbaae3eca338ca883cceaa20ba287a
Abcedi Ilacas <145126348+abcedi@users.noreply.github.com>
2024-01-27 12:27:26 -0500

    Started workshop 2

--- WS02/lab/Assessment.h
+++ WS02/lab/Assessment.h
@@ -1,1 +1,16 @@
+#ifndef SENECA_FILE_H
+#define SENECA_FILE_H
 
+namespace seneca {
+	struct Assessment {
+		double* m_mark;
+		char* m_title;
+	};
+	bool read(int& value, FILE* fptr);
+	bool read(double& value, FILE* fptr);
+	bool read(char* cstr, FILE* fptr);
+	bool read(Assessment& assess, FILE* fptr);
+	void freeMem(Assessment*& aptr, int size);
+	int read(Assessment*& aptr, FILE* fptr);
+}
+#endif
